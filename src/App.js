5d25a1e4edddf4f9035bfc335470a77b262cafc3
rdunetz <ryandunetz@gmail.com>
2024-05-28 13:48:23 -0400

    got bars and button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const bars = [];
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+
+  const random = () => {
+
+    const heights = [];
+
+    console.log("random");
+
+    // for (let i = 0; i < 10; i++) {
+    //   heights[i] = getRandomInt(i);
+    // }
+
+    const barHolder = document.getElementById('barHolder');
+
+    // console.log(barHolder.childNodes);
+
+    if (barHolder.childNodes.length === 0)  {
+
+      for(let i = 1; i < 11; i++){
+        const bar = document.createElement( 'div' );
+        bar.id = `bar_${i}`;
+        bar.classList.add('1');
+        barHolder.appendChild(bar);
+        bar.style.width = 50 + 'px';
+        bar.style.height = (Math.random() * 100) + 'px';
+        bar.style.backgroundColor = 'black';
+        bars.push(bar);
+
+      }
+    }
+
+    console.log(bars);
+
+    console.log(bars[0]);
+
+  }
+
+  const quickSort = (arr) => {
+    if (arr.length <= 1) {
+      return arr;
+    }
+
+    let pivot = arr[0];
+    let left = [];
+    let right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      }
+    }
+
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="barHolder" className='barHolder'></div>
+        <button onClick={random}>Generate Random Array</button>
+
       </header>
     </div>
   );
